1e4df4af0bacff8cc9b62780f79ce2d2aa8a1426
Mikail ÖZTÜRK <75627147+MIKAIL-OZTURK@users.noreply.github.com>
2021-10-06 22:14:20 +0300

    Embedded C Projects with STM32F407VG

--- 009_0_STD_USART/src/main.c
+++ 009_0_STD_USART/src/main.c
@@ -28,21 +28,24 @@ void GPIO_Config()
 
 void USART_Config()
 {
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);		// USART2 için clock hattýný aktif ettik.
 
-	USART_InitStruct.USART_BaudRate = 9600;
-	USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-	USART_InitStruct.USART_Mode = USART_Mode_Tx;
-	USART_InitStruct.USART_Parity = USART_Parity_No;
-	USART_InitStruct.USART_StopBits = USART_StopBits_1;
-	USART_InitStruct.USART_WordLength = USART_WordLength_8b;
+	USART_InitStruct.USART_BaudRate = 9600;		// BaudRate ->  Veri gönderme hýzýdýr. Aldýðýmýýz ve gönderdiðimiz veri hýzlarý eþit olmalýdýr.
+	USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None; // Donaným akýþ kontrolünü belirtir.Akýþ Kontrolü,
+	//	yavaþ ve hýzlý cihazlarýn veri kaybetme riski olmadan USART üzerinden birbirleriyle iletiþim kurmasý için bir yöntemdir.Akýþ kontrolü, vericiye iletimi
+	//	durdurmasý (duraklatma) veya baþlatmasý (devam ettirmesi) gerektiðini bildirmek için ekstra sinyal saðlar. Donaným akýþ kontrolü ise kablolardaki mantýk
+	//	seviyesinin, vericinin veri göndermeye devam edip etmeyeceðini veya duracaðýný belirlediði ekstra kablolar kullanýr.
+	USART_InitStruct.USART_Mode = USART_Mode_Tx; 	// USART modülünü hangi modda kullanacaðýmýzý seçiyoruz.Tx - Mesaj Gönderme (PC->MCU) ~ Rx - Mesaj Alma (MCU->PC)
+	USART_InitStruct.USART_Parity = USART_Parity_No; // Veri bitlerinde herhangi bir hata olup olmadýðýný alýcý cihaza bildirir.
+	USART_InitStruct.USART_StopBits = USART_StopBits_1;	// Haberleþmenin sonlanmasý için gereken 'Stop Biti'nin deðeri kaç bit olsun? 1 Bit olarak ayarladýk.
+	USART_InitStruct.USART_WordLength = USART_WordLength_8b; // Veri keliLme uzunluðunu belirtiriz.
 
 	USART_Init(USART2, &USART_InitStruct);
 
-	USART_Cmd(USART2, ENABLE);
+	USART_Cmd(USART2, ENABLE);		// USART bir çevresel birimi olduðundan aktif ediyoruz.
 }
 
-void USART_Puts(USART_TypeDef* USARTx, volatile char *s)
+void USART_Puts(USART_TypeDef* USARTx, volatile char *s)		// USART Mesaj Gönderme Fonksiyonu
 {
 	while(*s)
 	{
@@ -60,7 +63,7 @@ int main(void)
   while (1)
   {
 	  sprintf(str, "Hello World\n");
-	  USART_Puts(USART2, str);
+	  USART_Puts(USART2, str);			// "Hello World" --> MCU
 	  delay(1680000);
   }
 }
